8967c8710b5d7bf7cff3e13b7072404512c33d7e
imRahul05 <rahulkumar20000516@gmail.com>
2024-03-06 17:37:13 +0530

    fk

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Web Developer",
+          "MERN Stack Developer",
+          "Student",
+          "Flutter"
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import chatify from "../../Assets/Projects/donation.png";
+import bitsOfCode from "../../Assets/Projects/portfolio.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Gift Of Hope"
+              description="A Donation app using flutter Framework ,Dart and Firebase and Payment integration using Razorpay API."
+              ghLink="https://github.com/imRahul05/Flutter_projects"
+              demoLink="#"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Rahul's PortFolio"
+              description="This is Dynamic My portfolio Website USing ReactJS."
+              ghLink="https://github.com/imRahul05/imRahul05"
+              demoLink="https://imrahul05.github.io/imRahul05/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
