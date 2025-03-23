da97a9fa7068a523ca1205e5c3972a2c94d3bdb4
imrahul05 <rahulkumar20000516@gmail.com>
2025-03-23 13:22:50 +0530

    Add new tech and tool icons to Techstack and Toolstack components; include Tailwind CSS, Netlify, GitHub, GitHub Copilot, and npm icons

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,6 +3,8 @@ import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import { SiFlutter } from "react-icons/si";
 import {
+  DiHtml5,
+  DiCss3,
   DiJavascript1,
   DiReact,
   DiNodejs,
@@ -15,12 +17,20 @@ import {
 } from "react-icons/di";
 import { BiLogoTypescript, } from "react-icons/bi";
 import { SiFirebase ,SiTypescript} from "react-icons/si";
+import { BiLogoTailwindCss } from "react-icons/bi";;
+
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus style={{ color: "#00599C" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 style={{ color: "#E34F26" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 style={{ color: "#1572B6" }} />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 style={{ color: "#F0DB4F" }} />
@@ -39,6 +49,9 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiBootstrap style={{ color: "#553C7B" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <BiLogoTailwindCss style={{ color: "#06B6D4" }} />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript style={{ color: "#007ACC" }} />
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,14 +5,25 @@ import {
   SiPostman,
   SiMacos,
   SiSlack,
-  SiVercel
+  SiVercel,
+  SiNetlify,
+  SiGithubcopilot,
 } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { VscCopilot } from "react-icons/vsc";
+import { FaNpm } from "react-icons/fa";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos style={{ background: "linear-gradient(to right, #000000, #434343)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }} />
+        <SiMacos
+          style={{
+            background: "linear-gradient(to right, #000000, #434343)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode style={{ color: "#007ACC" }} />
@@ -24,7 +35,22 @@ function Toolstack() {
         <SiSlack style={{ color: "#4A154B" }} />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel style={{ color: "#000000" }} />
+        <SiVercel style={{ color: "grey" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify
+          style={{color: "#00C7B7"}}
+        />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGithub style={{ color: "grey" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <VscCopilot style={{ color: "grey" }} />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaNpm style={{ color: "#CB3837" }} />
       </Col>
     </Row>
   );
